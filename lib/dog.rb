befac614eb131ab74a801111da5d969650948e14
Miles Higbie <miles.higbie@gmail.com>
2020-01-08 18:49:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Add your code here
+require "pry"
+
+class Dog 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all.push(self)
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.clear_all 
+    @@all.clear
+  end 
+  
+  def self.print_all
+    @@all.each do |name|
+      binding.pry 
+    end 
+  end 
+end 
